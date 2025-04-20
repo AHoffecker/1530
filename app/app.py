2812a75541c67f55f2f78615cf872f1787e6c5d8
Hannah Kwee <mimi-hannah@usa.net>
2025-04-19 20:00:34 -0400

    Updated logic flow for app.py

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ def add_restaurants():
         "Ft. Pitt Subs", "PA Taco Co.", "Pom & Honey", "Shake Smart", "Steel City Kitchen", 
         "The Delicatessen", "The Eatery", "The Perch", "The Roost", "True Burger", "Wicked Pie"
     ]
-    
+
     for name in restaurants:
         # Check if the restaurant already exists to avoid duplicates
         if not Restaurant.query.filter_by(name=name).first():
@@ -38,7 +38,7 @@ def add_restaurants():
                 password=None
             )
             db.session.add(restaurant)
-    
+
     db.session.commit()
 
 # Create tables if they do not exist (done once when the app starts up)
@@ -60,9 +60,16 @@ def search():
 
 @app.route('/search_results', methods=['POST'])
 def search_results():
-    max_wait = request.form.get('time', type=str)
+    time = request.form.get('time', type=str)
+    print(f"time: \'{time}\'")
+    max_wait = 0
+    try:
+        max_wait = int(time)
+    except:
+        max_wait = ''
+    print(f"max_wait: \'{max_wait}\'")
 
-    if max_wait is not None:
+    if max_wait is not None or max_wait != '':
         # Calculate the average wait time for each restaurant
         avg_wait_times = db.session.query(
             WaitTime.restaurant_id,
@@ -70,16 +77,24 @@ def search_results():
         ).group_by(WaitTime.restaurant_id).subquery()
 
         # Join Restaurant and the average wait times to get restaurant names
-        restaurants_with_avg_wait = db.session.query(
-            Restaurant.name,
-            avg_wait_times.c.avg_wait
-        ).join(
-            avg_wait_times, Restaurant.id == avg_wait_times.c.restaurant_id
-        ).filter(
-            avg_wait_times.c.avg_wait <= max_wait
-        ).all()
+        if max_wait == "any":
+            restaurants_with_avg_wait = db.session.query(
+                Restaurant.name,
+                avg_wait_times.c.avg_wait
+            ).join(
+                avg_wait_times, Restaurant.id == avg_wait_times.c.restaurant_id
+            ).filter(
+                avg_wait_times.c.avg_wait <= max_wait
+            ).all()
+        else:
+            restaurants_with_avg_wait = db.session.query(
+                Restaurant.name,
+                avg_wait_times.c.avg_wait
+            ).join(
+                avg_wait_times, Restaurant.id == avg_wait_times.c.restaurant_id
+            )
 
-        return render_template('search_results.html', max_wait=max_wait, restaurants=restaurants_with_avg_wait)
+        return render_template('search_results.html', max_wait=time, restaurants=restaurants_with_avg_wait)
     else:
         return render_template('search_results.html', error="Please enter a valid maximum wait time.")
 
